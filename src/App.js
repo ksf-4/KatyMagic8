7f6627a6190c581af7e7a9cbf2b59088ac3e5f99
ksf-4 <129898286+ksf-4@users.noreply.github.com>
2023-04-14 13:48:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './magic8.css';
 
 function App() {
+
+const [userInput, setUserInput] = useState('');
+const [randomIndex, setRandomIndex] = useState('');
+const [error, setError] = useState('');
+
+const handleChange = (event) => {
+  event.preventDefault();
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+}
+//psuedo code for checking users question mark
+//check value of user input
+//check if user input string contains question mark
+//proceed as normal
+//if the user input does not contain a question mark 
+//show error
+const handleClick = () => {
+
+  if(userInput)
+  {
+    setError('')
+  setRandomIndex(Math.round(Math.random() * 20));
+  setUserInput('');
+  }
+  else{
+    setError('Please Enter a good question!')
+  }
+  console.log(randomIndex);
+}
+
+const possibleAnswers = [
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes, definitely',
+  'You may rely on it',
+  'As I see it, yes',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
+  'Reply hazy try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  'Don\'t count on it',
+  'My reply is no',
+  'My sources say no',
+  'Most likely',
+  'Outlook not so good',
+  'Very doubtful'
+];
+const answer = possibleAnswers[randomIndex];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to ask Magic 8-ball</h1>
+      <p className="info">Ask a question and tap me for an answer...</p>
+      <input type="text" className="question" value={userInput} onChange={handleChange} />
+      <div className="eight-ball">
+        <div className="content" onClick={handleClick}>
+        {error ? <p className="error">{error}</p> :
+          answer ? <p className="answer">{answer}</p> : <p className="eight">8</p>}
+          
+        </div>
+      </div>
     </div>
   );
 }
